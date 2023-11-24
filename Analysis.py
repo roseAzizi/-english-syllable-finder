339376c34ddfdba1050a8036611b544b654f9e80
roseAzizi <roseazizi.1718@gmail.com>
2023-11-24 13:24:52 -0500

    reworked things a bit

---
+++ Analysis.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib as plt   
+import tensorflow as tf 
+from tensorflow import keras 
+from tensorflow.keras import layers
+
+hyp_data = pd.read_pickle('hyp_data.pkl')
